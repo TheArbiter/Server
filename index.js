b6192c1374d473f08787b22f5a71bafdbbfa33fe
TheArbiter <ryan_ms28@hotmail.com>
2021-02-01 18:53:17 +1100

    modify file structure

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const path = require('path');
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function (req, res){
-    res.sendFile(path.join(__dirname + '/index.html'));
+    console.log("enter app.get");
+    res.sendFile(path.join(__dirname + '/src/index.html'));
     // res.send('<h1> Hello World</h1>');
 });
 
@@ -31,18 +32,25 @@ sql.connect(config, function (err) {
     let rating=req.body.warranty;
     console.log(rating);
 
-    let sqlQuery = 'Select Item1, Item2, Item3 from Items where Item4=' + parseInt(rating);
+    let sqlQuery = 'Select item1, item2, item3 from Items where item1=' + parseInt(rating);
 
     sqlRequest.query(sqlQuery, function (err, data) {
         if (err) console.log(err)
 
-            console.table(data.recordset);
-            console.log(data);
-            console.log(data.recordset);
-            console.log(data.rowsAffected);
-            console.log(data.recordset[0]);
-                    
-            res.send(data);
+            // console.table(data.recordset);
+            // console.log(data);
+            // console.log(data.recordset);
+            // console.log(data.rowsAffected);
+            // console.log(data.recordset[0]);
+            
+            let h='<h1 style="background:red;color:whitesmoke;margin:20px; border:20px solid red;">Employee Rating Board<h1>'
+            let str='<table style="margin-left:20px;">';
+            let row='';
+            for (let j=0;j<data.recordset.length;j++){
+                row= row + '<tr>' + '<td style="width:150px;">' + data.recordset[j].item1 + '</td>' + '<td style="width:150px;">'
+            }    
+            str=str + row + '</table>';
+            res.send(h+str);
     sql.close();
 
     });
